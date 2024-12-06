e041472e5deb633146d12190e7cc939b041b9afa
Grayson Keaton <gkeaton23@gmail.com>
2024-12-05 20:59:16 -0500

    added header

--- frontend/family-flashback/src/App.jsx
+++ frontend/family-flashback/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Header from './components/header';
 
 function App() {
 
   return (
     <>
-      <h1>Family Flashback</h1>
+    <Header />
+    <h1>Family Flashback</h1>
     </>
   )
 }
