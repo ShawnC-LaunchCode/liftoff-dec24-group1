e7ccba0e0808afb243e91466e9e2bf6db4751d43
Grayson Keaton <gkeaton23@gmail.com>
2024-12-07 10:19:47 -0500

    Initial Spring Boot setup

---
+++ backend/src/main/java/com/familyflashback/familyflashback_backend/FamilyflashbackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.familyflashback.familyflashback_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyflashbackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyflashbackBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/familyflashback/familyflashback_backend/FamilyflashbackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.familyflashback.familyflashback_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyflashbackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
