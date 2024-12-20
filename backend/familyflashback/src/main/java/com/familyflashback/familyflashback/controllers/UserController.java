25ecadd10160bcfb64069cc073dbe7c0b30ff989
Lori <148814155+Lbeckap@users.noreply.github.com>
2024-12-19 21:40:11 -0500

    started user endpoint

--- backend/familyflashback/src/main/java/com/familyflashback/familyflashback/controllers/UserController.java
+++ backend/familyflashback/src/main/java/com/familyflashback/familyflashback/controllers/UserController.java
@@ -3,7 +3,9 @@ package com.familyflashback.familyflashback.controllers;
 
 import com.familyflashback.familyflashback.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +15,9 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+    @PostMapping("/user")
+    public String createUser() {  // use @RequestParam
+        return "";
+    }
 
 }
