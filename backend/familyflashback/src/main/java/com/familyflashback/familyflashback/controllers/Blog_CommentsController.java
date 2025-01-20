0ce28dcab86781add1fd7d0bae81915d48d76a66
Lori <148814155+Lbeckap@users.noreply.github.com>
2025-01-20 17:12:44 -0500

    blog and blog_comments endpoints

---
+++ backend/familyflashback/src/main/java/com/familyflashback/familyflashback/controllers/Blog_CommentsController.java
@@ -0,0 +1,40 @@
+package com.familyflashback.familyflashback.controllers;
+
+import com.familyflashback.familyflashback.models.Blog_Comments;
+import com.familyflashback.familyflashback.models.data.Blog_CommentsRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("comments")
+public class Blog_CommentsController {
+
+    @Autowired
+    Blog_CommentsRepository blog_commentsRepository;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Blog_Comments>> getAllComments (@PathVariable("id") String id) {
+        List<Blog_Comments> comments = blog_commentsRepository.findAllByUserId(id);
+        if (!comments.isEmpty()) {
+            return ResponseEntity.ok(comments);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Blog_Comments> addComment (@Valid @RequestBody Blog_Comments blog_comment){
+
+        Blog_Comments comment = blog_commentsRepository.save(blog_comment);
+        return new ResponseEntity<>(comment, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test (){
+        return ResponseEntity.ok("Test endpoint success");
+    }
+}
---
+++ backend/familyflashback/src/main/java/com/familyflashback/familyflashback/models/Blog_Comments.java
@@ -0,0 +1,66 @@
+package com.familyflashback.familyflashback.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+public class Blog_Comments extends AbstractEntity {
+
+    @Column(name = "user_id")
+    private String userId;
+
+    @Size(max = 500, message = "Cannot exceed 500 characters")
+    @NotNull
+    private String body;
+
+    @Column(name = "update_dt", insertable = false)
+    private LocalDateTime dateUpdated;
+
+    public Blog_Comments() {}
+
+    public Blog_Comments(String userId, String body) {
+        this.userId = userId;
+        this.dateUpdated = LocalDateTime.now();
+        this.body = body;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(LocalDateTime dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+
+    public @Size(max = 500, message = "Cannot exceed 500 characters") @NotNull String getBody() {
+        return body;
+    }
+
+    public void setBody(@Size(max = 500, message = "Cannot exceed 500 characters") @NotNull String body) {
+        this.body = body;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Blog_Comments{" +
+                "userId='" + userId + '\'' +
+                ", body='" + body + '\'' +
+                ", dateUpdated=" + dateUpdated +
+                '}';
+    }
+}
