a1379a4e588c10b0c6f76046a89a14a079250d7f
Tori Rodgers <torirodgers22@gmail.com>
2025-01-22 21:18:41 -0600

    fully working comment system with reply, edit, update, delete with api file. fetching is next

--- frontend/family-flashback/src/components/family_history/Comment.jsx
+++ frontend/family-flashback/src/components/family_history/Comment.jsx
@@ -1,7 +1,34 @@
 import "./commentSystem.css";
 import UserIcon from"../assets/user-icon.png";
+import CommentForm from "./CommentForm";
 
-const Comment = ({comment}) => {
+const Comment = ({
+  comment,
+  replies,
+  setActiveComment,
+  activeComment,
+  updateComment,
+  deleteComment,
+  addComment,
+  parentId = null,
+  currentUserId,
+}) => {
+
+  const fiveMinutes = 300000;
+  const timePassed = new Date() - new Date(comment.createdAt) > fiveMinutes;
+  const canReply = Boolean(currentUserId);
+  const canEdit = currentUserId === comment.userId && !timePassed;
+  const canDelete = currentUserId === comment.userId && !timePassed;
+  const createdAt = new Date(comment.createdAt).toLocaleDateString();
+  const isReplying = 
+    activeComment && 
+    activeComment.type === 'replying' &&
+    activeComment.id === comment.id;
+  const isEditing = 
+    activeComment && 
+    activeComment.type === 'editing' &&
+    activeComment.id === comment.id;
+  const replyId = parentId ? parentId : comment.id;
 
   return (
     <div className="comment">
@@ -11,12 +38,60 @@ const Comment = ({comment}) => {
       <div className="comment-right-part">
         <div className="comment-content">
           <div className="comment-author">{comment.username}</div>
-          <div>{comment.createdAt}</div>
+          <div>{createdAt}</div>
         </div>
-        <div className="comment-text">{comment.body}</div>
+        {!isEditing && <div className="comment-text">{comment.body}</div>}
+        {isEditing && (
+          <CommentForm 
+            submitLabel="Update" 
+            hasCancelButton 
+            intialText={comment.body}
+            handleSubmit={(text) => updateComment(text, comment.id)} 
+            handleCancel={() => setActiveComment(null)}
+            />
+        )}
+        <div className="comment-actions">
+          {canReply && (
+            <div className="comment-action"
+            onClick={() => 
+              setActiveComment({ id: comment.id, type: "replying"})
+              }>Reply</div>)}
+          {canEdit && (
+            <div className="comment-action"
+            onClick={() => 
+              setActiveComment({ id: comment.id, type: "editing"})
+              }>Edit</div>)}
+          {canDelete && (
+            <div className="comment-action" 
+            onClick={() => 
+              deleteComment(comment.id)}>Delete</div>)}
+        </div>
+        {isReplying && (
+          <CommentForm 
+            submitLabel="Reply" 
+            handleSubmit={(text) => addComment(text, replyId)} />
+        )}
+        {replies.length > 0 && (
+          <div className="replies">
+            {replies.map((reply) => (
+              <Comment 
+                comment={reply} 
+                key={reply.id} 
+                replies={[]} 
+                currentUserId={currentUserId}
+                deleteComment={deleteComment}
+                updateComment={updateComment}
+                addComment={addComment}
+                activeComment={activeComment}
+                setActiveComment={setActiveComment}
+                parentId={comment.id}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
-  )
+  );
 
 };
 
--- frontend/family-flashback/src/components/family_history/Comments.jsx
+++ frontend/family-flashback/src/components/family_history/Comments.jsx
@@ -1,15 +1,64 @@
 import { useEffect, useState } from "react";
-import { getComments as getCommentsApi } from "./api";
 import "./commentSystem.css";
 import Comment from "./Comment";
+import CommentForm from "./CommentForm";
+import { 
+    getComments as getCommentsApi, 
+    createComment as createCommentApi, 
+    deleteComment as deleteCommentApi,
+    updateComment as updateCommentApi} from "./api";
+
 
 const Comments = ({currentUserId}) => {
 
-    const[backendComments, setBackendComments] = useState([])
+    const[backendComments, setBackendComments] = useState([]);
+    
+    const[activeComment, setActiveComment] = useState(null);
+
     const rootComments = backendComments.filter(
         (backendComment) => backendComment.parentId === null
     );
-    console.log("backendComments", backendComments);
+    
+    const getReplies = commentId => {
+        return backendComments.filter(backendComment => backendComment.parentId === commentId).sort((a, 
+            b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+        );
+    };
+
+    const addComment = (text, parentId) => {
+        console.log("addComment", text, parentId);
+        // fetch goes here
+        createCommentApi(text, parentId).then(comment => {
+            setBackendComments([comment, ...backendComments]);
+            setActiveComment(null);
+        });
+    };
+
+    const deleteComment = (commentId) => {
+        //fetch goes here as well i think to update database
+        if (window.confirm('Are you sure you want to delete this comment?')) {
+            deleteCommentApi(commentId).then(() => {
+                const updatedBackendComments = backendComments.filter(
+                    (backendComment) => backendComment.id !== commentId
+                );
+                setBackendComments(updatedBackendComments);
+            });
+        };
+    };
+
+    const updateComment = (text, commentId) => {
+        //fetch here as well
+        updateCommentApi(text, commentId).then(() =>{
+            const updatedBackendComments = backendComments.map(backendComment => {
+                if (backendComment.id === commentId) {
+                    return {...backendComment, body: text };
+                    };
+                return backendComment;
+            });
+            setBackendComments(updatedBackendComments);
+            setActiveComment(null);
+        });
+    };
 
     useEffect(() => {
         // fetch from api will go here
@@ -22,9 +71,21 @@ const Comments = ({currentUserId}) => {
     return (
         <div className="comments">
             <h3 className="comments-title">Comments</h3>
+            <div className="comment-form-title">Write comment</div>
+            <CommentForm submitLabel="Write" handleSubmit={addComment} />
             <div className="comments-container">
                 {rootComments.map((rootComment) => (
-                    <Comment key={rootComment.id} comment={rootComment} />
+                    <Comment 
+                    key={rootComment.id} 
+                    comment={rootComment} 
+                    replies={getReplies(rootComment.id)}
+                    currentUserId={currentUserId}
+                    deleteComment={deleteComment}
+                    updateComment={updateComment}
+                    activeComment={activeComment}
+                    setActiveComment={setActiveComment}
+                    addComment={addComment}
+                    />
                 ))}
 
             </div>
