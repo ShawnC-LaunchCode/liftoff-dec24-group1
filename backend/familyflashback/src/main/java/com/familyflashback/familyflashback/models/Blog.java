c1193a6eb864eb54964f2e3600aa64f5a7fcfbb7
Tori Rodgers <torirodgers22@gmail.com>
2025-01-29 19:01:59 -0600

    updated blog_comments model and sql file for id and parentId

--- backend/familyflashback/src/main/java/com/familyflashback/familyflashback/models/Blog.java
+++ backend/familyflashback/src/main/java/com/familyflashback/familyflashback/models/Blog.java
@@ -1,6 +1,8 @@
 package com.familyflashback.familyflashback.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
--- backend/familyflashback/src/main/java/com/familyflashback/familyflashback/models/Blog_Comments.java
+++ backend/familyflashback/src/main/java/com/familyflashback/familyflashback/models/Blog_Comments.java
@@ -2,6 +2,8 @@ package com.familyflashback.familyflashback.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -12,23 +14,40 @@ import java.time.LocalDateTime;
 @Entity
 public class Blog_Comments extends AbstractEntity {
 
-    @Column(name = "user_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "parentId", nullable = true)
+    private Integer parentId;
+
+    @Column(name = "userId")
     private String userId;
 
     @Size(max = 500, message = "Cannot exceed 500 characters")
     @NotNull
     private String body;
 
-    @Column(name = "update_dt")
+    @Column(name = "createdAt")
     private LocalDateTime dateUpdated;
 
     public Blog_Comments() {}
 
-    public Blog_Comments(String userId, String body) {
+    public Blog_Comments(String userId, String body, Integer parentId) {
         this.userId = userId;
         this.dateUpdated = LocalDateTime.now();
         this.body = body;
+        this.parentId = parentId;
     }
+
+    public int getCommentId() { return id; }
+
+    public void setCommentId(int id) { this.id = id; }
+
+    public Integer getParentId() { return parentId; }
+
+    public void setParentId(Integer parentId) { this.parentId = parentId; }
 
     public String getUserId() {
         return userId;
@@ -58,9 +77,11 @@ public class Blog_Comments extends AbstractEntity {
     @Override
     public String toString() {
         return "Blog_Comments{" +
-                "userId='" + userId + '\'' +
+                "id=" + id +
+                ", userId='" + userId + '\'' +
                 ", body='" + body + '\'' +
                 ", dateUpdated=" + dateUpdated +
+                ", parentId=" + parentId +
                 '}';
     }
 }
