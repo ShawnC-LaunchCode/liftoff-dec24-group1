969de84c63df18c6a874cef895a1e369cfa6e1f8
Tori Rodgers <torirodgers22@gmail.com>
2025-01-29 20:48:45 -0600

    updated fetch to call the correct endpoint for add, update, delete

--- frontend/family-flashback/src/components/family_history/Comments.jsx
+++ frontend/family-flashback/src/components/family_history/Comments.jsx
@@ -2,96 +2,99 @@ import { useEffect, useState } from "react";
 import "./commentSystem.css";
 import Comment from "./Comment";
 import CommentForm from "./CommentForm";
-import { 
-    getComments as getCommentsApi, 
-    createComment as createCommentApi, 
-    deleteComment as deleteCommentApi,
-    updateComment as updateCommentApi} from "./api";
 
+const Comments = ({ currentUserId }) => {
+  const [backendComments, setBackendComments] = useState([]);
 
-const Comments = ({currentUserId}) => {
+  const [activeComment, setActiveComment] = useState(null);
 
-    const[backendComments, setBackendComments] = useState([]);
-    
-    const[activeComment, setActiveComment] = useState(null);
+  const rootComments = backendComments.filter(
+    (backendComment) => backendComment.parentId === null
+  );
 
-    const rootComments = backendComments.filter(
-        (backendComment) => backendComment.parentId === null
-    );
-    
-    const getReplies = commentId => {
-        return backendComments.filter(backendComment => backendComment.parentId === commentId).sort((a, 
-            b) => new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
-        );
-    };
+  const getReplies = (id) => {
+    return backendComments
+      .filter((backendComment) => backendComment.parentId === id)
+      .sort(
+        (a, b) =>
+          new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime()
+      );
+  };
 
-    const addComment = (text, parentId) => {
-        console.log("addComment", text, parentId);
-        // fetch goes here
-        createCommentApi(text, parentId).then(comment => {
-            setBackendComments([comment, ...backendComments]);
-            setActiveComment(null);
-        });
-    };
+  const fetchComments = async () => {
+    const response = await fetch("http://localhost:8080/comments");
+    const data = await response.json();
+    setBackendComments(data);
+  };
 
-    const deleteComment = (commentId) => {
-        //fetch goes here as well i think to update database
-        if (window.confirm('Are you sure you want to delete this comment?')) {
-            deleteCommentApi(commentId).then(() => {
-                const updatedBackendComments = backendComments.filter(
-                    (backendComment) => backendComment.id !== commentId
-                );
-                setBackendComments(updatedBackendComments);
-            });
-        };
-    };
+  const addComment = async (text, parentId) => {
+    const response = await fetch("http://localhost:8080/comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ body: text, parentId }),
+    });
+    const comment = await response.json();
+    setBackendComments([comment, ...backendComments]);
+    setActiveComment(null);
+  };
+  const deleteComment = async (id) => {
+    if (window.confirm("Are you sure you want to delete this comment?")) {
+      await fetch(`http://localhost:8080/comments/${id}`, {
+        method: "DELETE",
+      });
+      const updatedBackendComments = backendComments.filter(
+        (backendComment) => backendComment.id !== id
+      );
+      setBackendComments(updatedBackendComments);
+    }
+  };
 
-    const updateComment = (text, commentId) => {
-        //fetch here as well
-        updateCommentApi(text, commentId).then(() =>{
-            const updatedBackendComments = backendComments.map(backendComment => {
-                if (backendComment.id === commentId) {
-                    return {...backendComment, body: text };
-                    };
-                return backendComment;
-            });
-            setBackendComments(updatedBackendComments);
-            setActiveComment(null);
-        });
-    };
+  const updateComment = async (text, id) => {
+    await fetch(`http://localhost:8080/comments/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ body: text }),
+    });
+    const updatedBackendComments = backendComments.map((backendComment) => {
+      if (backendComment.id === id) {
+        return { ...backendComment, body: text };
+      }
+      return backendComment;
+    });
+    setBackendComments(updatedBackendComments);
+    setActiveComment(null);
+  };
 
-    useEffect(() => {
-        // fetch from api will go here
-        getCommentsApi().then((data) => {
-            setBackendComments(data)
-        });
+  useEffect(() => {
+    fetchComments();
+  }, []);
 
-    }, []);
+  return (
+    <div className="comments">
+      <h3 className="comments-title">Comments</h3>
 
-    return (
-        <div className="comments">
-            <h3 className="comments-title">Comments</h3>
-            
-            <CommentForm submitLabel="Comment" handleSubmit={addComment} />
-            <div className="comments-container">
-                {rootComments.map((rootComment) => (
-                    <Comment 
-                    key={rootComment.id} 
-                    comment={rootComment} 
-                    replies={getReplies(rootComment.id)}
-                    currentUserId={currentUserId}
-                    deleteComment={deleteComment}
-                    updateComment={updateComment}
-                    activeComment={activeComment}
-                    setActiveComment={setActiveComment}
-                    addComment={addComment}
-                    />
-                ))}
-
-            </div>
-        </div>
-    );
-
+      <CommentForm submitLabel="Comment" handleSubmit={addComment} />
+      <div className="comments-container">
+        {rootComments.map((rootComment) => (
+          <Comment
+            key={rootComment.id}
+            comment={rootComment}
+            replies={getReplies(rootComment.id)}
+            currentUserId={currentUserId}
+            deleteComment={deleteComment}
+            updateComment={updateComment}
+            activeComment={activeComment}
+            setActiveComment={setActiveComment}
+            addComment={addComment}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Comments;
