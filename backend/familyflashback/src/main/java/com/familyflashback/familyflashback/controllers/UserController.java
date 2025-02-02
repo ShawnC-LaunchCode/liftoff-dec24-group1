13da94b0a3f8c7c25dbc1a7f439240ab4b58e915
Grayson Keaton <gkeaton23@gmail.com>
2025-02-02 08:28:13 -0700

    endpoints not checking session bug

--- backend/familyflashback/src/main/java/com/familyflashback/familyflashback/controllers/UserController.java
+++ backend/familyflashback/src/main/java/com/familyflashback/familyflashback/controllers/UserController.java
@@ -1,126 +1,122 @@
- package com.familyflashback.familyflashback.controllers;
+package com.familyflashback.familyflashback.controllers;
 
- import com.familyflashback.familyflashback.models.Person;
- import com.familyflashback.familyflashback.models.User;
- import com.familyflashback.familyflashback.models.data.PersonRepository;
- import com.familyflashback.familyflashback.models.data.UserRepository;
- import jakarta.validation.Valid;
- import org.springframework.beans.factory.annotation.Autowired;
- import org.springframework.http.HttpStatus;
- import org.springframework.http.ResponseEntity;
- import org.springframework.web.bind.annotation.*;
+import com.familyflashback.familyflashback.models.Person;
+import com.familyflashback.familyflashback.models.User;
+import com.familyflashback.familyflashback.models.data.PersonRepository;
+import com.familyflashback.familyflashback.models.data.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
- import java.util.HashMap;
- import java.util.Map;
- import java.util.Optional;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
- @RestController
- @RequestMapping("user")
- public class UserController {
+@RestController
+@RequestMapping("user")
+public class UserController {
 
-      @Autowired
-      UserRepository userRepository;
+    @Autowired
+    UserRepository userRepository;
 
-      @Autowired
-      PersonRepository personRepository;
+    @Autowired
+    PersonRepository personRepository;
 
-      @Autowired
-      SessionController sessionController;
+    @Autowired
+    SessionController sessionController;
 
-      @PostMapping
-      public ResponseEntity<Map<String, Object>> createUser(@Valid @RequestBody User user, @CookieValue(name = "session", required = false) String cookieValue) {
+    @PostMapping
+    public ResponseEntity<Map<String, Object>> createUser(@Valid @RequestBody User user, @CookieValue(name = "session", required = false) String cookieValue) {
 
-          if(cookieValue != null) {
-              if (sessionController.sessionRepository.findById(cookieValue).isPresent()) {
-                  return ResponseEntity.notFound().build();
-              }
-          }
+        if(cookieValue != null) {
+            if (sessionController.sessionRepository.findById(cookieValue).isPresent()) {
+                return ResponseEntity.notFound().build();
+            }
+        }
 
-          Map<String, Object> createdResponse = new HashMap<>();
+        Map<String, Object> createdResponse = new HashMap<>();
 
-          if(userRepository.findByEmail(user.getEmail()).isPresent()) {
-              createdResponse.put("error", "email already in use");
-              return new ResponseEntity<>(createdResponse, HttpStatus.ACCEPTED);
-          }
+        if(userRepository.findByEmail(user.getEmail()).isPresent()) {
+            createdResponse.put("error", "email already in use");
+            return new ResponseEntity<>(createdResponse, HttpStatus.ACCEPTED);
+        }
 
-            user.hashPass();
-            User createdUser = userRepository.save(user);
-            Person personCopy = new Person();
-            personCopy.setName(user.getName());
-            personCopy.setUser(user);
-            Person createdPerson = personRepository.save(personCopy);
-            System.out.println("Person Id for user " + createdUser.getId() + " is " + createdPerson.getId());
+        user.hashPass();
+        User createdUser = userRepository.save(user);
+        Person personCopy = new Person();
+        personCopy.setName(user.getName());
+        personCopy.setUser(user);
+        Person createdPerson = personRepository.save(personCopy);
+        System.out.println("Person Id for user " + createdUser.getId() + " is " + createdPerson.getId());
 
-            createdUser.setPersonID(createdPerson.getId());
-            userRepository.save(createdUser);
-            String sessionId = sessionController.setUserInSession(createdUser);
+        createdUser.setPersonID(createdPerson.getId());
+        userRepository.save(createdUser);
+        String sessionId = sessionController.setUserInSession(createdUser);
 
-            createdResponse.put("createdUser", createdUser);
-            createdResponse.put("createdPerson", createdPerson);
-            createdResponse.put("session", sessionId);
+        createdResponse.put("createdUser", createdUser);
+        createdResponse.put("createdPerson", createdPerson);
+        createdResponse.put("session", sessionId);
 
-            return new ResponseEntity<>(createdResponse, HttpStatus.CREATED);
-      }
+        return new ResponseEntity<>(createdResponse, HttpStatus.CREATED);
+    }
 
-      @GetMapping("/{id}")
-      public ResponseEntity<User> getUser(@PathVariable("id") String Id) {
-          if (sessionController.isSessionActive(Id)) {
-              Optional<User> user = userRepository.findById(Id);
-              return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-          }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") String Id) {
+        Optional<User> user = userRepository.findById(Id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-          return ResponseEntity.notFound().build();
-      }
+    @PatchMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> updateUser(@PathVariable("id") String Id, @Valid @RequestBody User updatedUser) {
+        Optional<User> user = userRepository.findById(Id);
 
-      @PatchMapping("/{id}")
-      public ResponseEntity<Map<String, Object>> updateUser(@PathVariable("id") String Id, @Valid @RequestBody User updatedUser) {
-          Optional<User> user = userRepository.findById(Id);
+        if (user.isPresent()) {
+            User existingUser = user.get();
+            Optional<Person> person = personRepository.findByUsersPersonId(existingUser.getPersonID());
+            Person existingPerson = person.get();
 
-          if (user.isPresent()) {
-              User existingUser = user.get();
-              Optional<Person> person = personRepository.findByUsersPersonId(existingUser.getPersonID());
-              Person existingPerson = person.get();
+            if (updatedUser.getName() != null) {
+                existingUser.setName(updatedUser.getName());
+                existingPerson.setName(updatedUser.getName());
+            }
+            if (updatedUser.getPassword() != null) {
+                existingUser.setPassword(updatedUser.getPassword());
+            }
+            if (updatedUser.getEmail() != null) {
+                existingUser.setEmail(updatedUser.getEmail());
+            }
 
-              if (updatedUser.getName() != null) {
-                  existingUser.setName(updatedUser.getName());
-                  existingPerson.setName(updatedUser.getName());
-              }
-              if (updatedUser.getPassword() != null) {
-                  existingUser.setPassword(updatedUser.getPassword());
-              }
-              if (updatedUser.getEmail() != null) {
-                  existingUser.setEmail(updatedUser.getEmail());
-              }
+            User savedUpdatedUser = userRepository.save(existingUser);
+            Person savedUpdatedPerson = personRepository.save(existingPerson);
 
-              User savedUpdatedUser = userRepository.save(existingUser);
-              Person savedUpdatedPerson = personRepository.save(existingPerson);
+            Map<String, Object> updatedResponse = new HashMap<>();
+            updatedResponse.put("updatedUser", savedUpdatedUser);
+            updatedResponse.put("updatedPerson", savedUpdatedPerson);
 
-              Map<String, Object> updatedResponse = new HashMap<>();
-              updatedResponse.put("updatedUser", savedUpdatedUser);
-              updatedResponse.put("updatedPerson", savedUpdatedPerson);
+            return new ResponseEntity<>(updatedResponse, HttpStatus.OK);
+        } else {
+            System.out.println("User " + Id + " does not exist.");
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-              return new ResponseEntity<>(updatedResponse, HttpStatus.OK);
-          } else {
-              System.out.println("User " + Id + " does not exist.");
-              return ResponseEntity.notFound().build();
-          }
-      }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") String Id) {
+        Optional<User> user = userRepository.findById(Id);
+        if (user.isPresent()) {
+            userRepository.deleteById(Id);
+            return ResponseEntity.noContent().build();
+        } else {
+            System.out.println("User " + Id + " does not exist.");
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-     @DeleteMapping("/{id}")
-     public ResponseEntity<User> deleteUser(@PathVariable("id") String Id) {
-         Optional<User> user = userRepository.findById(Id);
-         if (user.isPresent()) {
-             userRepository.deleteById(Id);
-             return ResponseEntity.noContent().build();
-         } else {
-             System.out.println("User " + Id + " does not exist.");
-             return ResponseEntity.notFound().build();
-         }
-     }
-
-     @GetMapping()
-     public ResponseEntity<String> testEndpoint() {
-         return ResponseEntity.ok("Test endpoint hit");
-     }
+    @GetMapping()
+    public ResponseEntity<String> testEndpoint() {
+        return ResponseEntity.ok("Test endpoint hit");
+    }
 
 }
--- frontend/family-flashback/src/components/home.jsx
+++ frontend/family-flashback/src/components/home.jsx
@@ -1,48 +1,40 @@
 import React from 'react'
 
 export default function Home() {
-  return (
-    <div className="terms-comp">
-        <h1>Terms and Conditions</h1>
+    const [userData, setUserData] = React.useState(null);
 
-        <ul>
-            <li>Lorem ipsum dolor sit amet.</li>
-            <li>Lorem ipsum dolor.</li>
-            <li>Lorem ipsum dolor sit amet, consectetur.</li>
-        </ul>
-        <br/>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Nam elit felis, condimentum eget neque id, gravida venenatis libero.
-        Nunc tincidunt eget turpis ac blandit.
-        Curabitur pellentesque nulla justo, viverra mattis urna volutpat sit amet.
-        Sed vel dolor ipsum. Nunc sit amet purus ut enim iaculis porta.
-        Sed in metus venenatis, rutrum massa a, sollicitudin diam. Ut non urna dolor.
-        Etiam ut sem dictum, varius orci quis, tempus mi.
-        Donec sem lorem, posuere quis porttitor eget, pulvinar non enim.
-        Maecenas venenatis vestibulum luctus. Ut dui justo, placerat ut orci quis, auctor congue arcu.
-        </p>
-        <br/>
-        <p>
-        Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-        Aenean commodo ligula eget dolor. Aenean massa.
-        Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-        Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
-        Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-        In enim justo, rhoncus ut, imperdiet a, venenatis
-        </p>
-        <br/>
-        <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Nam elit felis, condimentum eget neque id, gravida venenatis libero.
-        Nunc tincidunt eget turpis ac blandit.
-        Curabitur pellentesque nulla justo, viverra mattis urna volutpat sit amet.
-        Sed vel dolor ipsum. Nunc sit amet purus ut enim iaculis porta.
-        Sed in metus venenatis, rutrum massa a, sollicitudin diam. Ut non urna dolor.
-        Etiam ut sem dictum, varius orci quis, tempus mi.
-        Donec sem lorem, posuere quis porttitor eget, pulvinar non enim.
-        Maecenas venenatis vestibulum luctus. Ut dui justo, placerat ut orci quis, auctor congue arcu.
-        </p>
-    </div>
-  )
+    React.useEffect(() => {
+        const fetchUserData = async () => {
+          const response = await fetch("http://localhost:8080/user/IBbCK-fV0FfqYzmMjeCr1", {
+              method: 'GET',
+              credentials: 'include',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              }
+          });
+
+          if (response.ok) {
+              const data = await response.json();
+              setUserData(data);
+          } else {
+              console.error("Failed to fetch user data");
+          }
+        };
+
+        fetchUserData();
+    }, []);
+
+    return (
+        <div>
+            {userData ? (
+                <div>
+                    <h2>User Information</h2>
+                    <pre>{JSON.stringify(userData, null, 2)}</pre>
+                </div>
+            ) : (
+                <p>Loading user data...</p>
+            )}
+        </div>
+    );
 }
